22eb5ccb6cb17cd04be4b71a84a5c22723597de4
SFlorin <stoflorin22@gmail.com>
2023-04-22 15:43:19 +0300

    Make Login Controller

---
+++ eCommerceApp-Backend/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using eCommerceApp_Backend.Interface;
+using eCommerceApp_Backend.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceApp_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public LoginController(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLoginDTO userCredentials)
+        {
+            if (userCredentials == null)
+                return BadRequest(ModelState);
+
+            var userExistent = _userRepository.GetUsers()
+                .Where(ue => ue.UserName == userCredentials.UserName && ue.Password == userCredentials.Password)
+                .FirstOrDefault();
+
+            if (userExistent == null)
+                return NotFound();
+
+            var returnedUser = _mapper.Map<UserDTO>(userExistent);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(returnedUser);
+
+        }
+
+    }
+}
---
+++ eCommerceApp-Backend/Models/DTO/UserLoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace eCommerceApp_Backend.Models.DTO
+{
+    public class UserLoginDTO
+    {
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
