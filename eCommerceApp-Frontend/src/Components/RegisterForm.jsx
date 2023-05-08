7e192e674c6d47449bac422083ad5c07b6cb7a8c
SFlorin <stoflorin22@gmail.com>
2023-05-08 16:51:40 +0300

    Make RegisterForm Component with MaterialUI

---
+++ eCommerceApp-Frontend/src/Components/RegisterForm.jsx
@@ -0,0 +1,151 @@
+import {Box, Button, Typography, TextField} from "@mui/material"
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
+const RegisterForm = () => {
+
+    const [firstname, updateFirstname] = useState("");
+    const [lastname, updatelastName] = useState("");
+    const [username, updateUsername] = useState("");
+    const [email, updateEmail] = useState("");
+    const [password, updatePassword] = useState("");
+
+    const validate = () => {
+        if((firstname === '' || firstname === null) && (lastname === '' || lastname === null) && (username === '' || username === null) && (email === '' || email === null) && (password === '' || password === null))
+        {
+            toast.warning("Missing inputs", {
+                position: toast.POSITION.TOP_CENTER
+            });
+            return false;
+        } else if(firstname === '' || firstname === null) {
+            toast.warning("Missing firstname", {
+                position: toast.POSITION.TOP_CENTER
+            });
+            return false;
+        } else if(lastname === '' || lastname === null) {
+            toast.warning("Missing lastname", {
+                position: toast.POSITION.TOP_CENTER
+            });
+            return false;
+        } else if(username === '' || username === null) {
+            toast.warning("Missing username", {
+                position: toast.POSITION.TOP_CENTER
+            });
+            return false;
+        } else if(email === '' || email === null) {
+            toast.warning("Missing email", {
+                position: toast.POSITION.TOP_CENTER
+            });
+            return false;
+        } else if(password === '' || password === null) {
+            toast.warning("Missing password", {
+                position: toast.POSITION.TOP_CENTER
+            });
+            return false;
+        }
+        return true;
+    }
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        if(validate()){
+            const inputObject = {
+                "firstname": firstname,
+                "lastname": lastname,
+                "userName" : username,
+                "email" : email,
+                "password" : password
+            };
+            console.log(inputObject);
+    }}
+
+    return (
+        <div>
+            <form onSubmit={handleRegister}>
+                <Box 
+                  display={"flex"} 
+                  flexDirection={"column"} 
+                  maxWidth={600}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  margin={"auto"}
+                  marginTop={5}
+                  padding={5}
+                  borderRadius={5}
+                  boxShadow={"5px 5px 10px #ccc"}
+                  sx={{":hover": {
+                    boxShadow:"10px 10px 20px #ccc"
+                  }}}
+                  >
+                    <Typography 
+                       variant="h5" 
+                       padding={5} 
+                       textAlign={"center"}
+                       >
+                        Register
+                    </Typography>
+                    <TextField 
+                       size="small" 
+                       margin={"normal"} 
+                       type="text" 
+                       variant="outlined" 
+                       label="Firstname"
+                       value={firstname} 
+                       onChange={e => updateFirstname(e.target.value)}
+                    />
+                    <TextField 
+                       size="small" 
+                       margin={"normal"} 
+                       type="text" 
+                       variant="outlined" 
+                       label="Lastname"
+                       value={lastname} 
+                       onChange={e => updatelastName(e.target.value)}
+                    />
+                    <TextField 
+                       size="small" 
+                       margin={"normal"} 
+                       type="text" 
+                       variant="outlined" 
+                       label="Username"
+                       value={username} 
+                       onChange={e => updateUsername(e.target.value)}
+                    />
+                    <TextField 
+                       size="small"
+                       margin={"normal"} 
+                       type="email" 
+                       variant="outlined" 
+                       label="Email"
+                       value={email}
+                       onChange={e => updateEmail(e.target.value)}
+                    />
+                    <TextField 
+                       size="small"
+                       margin={"normal"} 
+                       type="password" 
+                       variant="outlined" 
+                       label="Password"
+                       value={password}
+                       onChange={e => updatePassword(e.target.value)}
+                    />
+                    <Button 
+                       variant="contained" 
+                       color="primary" 
+                       type="submit"
+                       sx={{ marginTop: 3, borderRadius: 5}}
+                        >
+                        Register
+                    </Button>
+                    <Button 
+                       sx={{ marginTop: 3, borderRadius: 5}}
+                        >
+                        Already have an account?
+                    </Button>
+                </Box>
+            </form>
+        </div>
+    );
+}
+
+export default RegisterForm;
--- eCommerceApp-Frontend/src/Routes/Register/Register.jsx
+++ eCommerceApp-Frontend/src/Routes/Register/Register.jsx
@@ -1,10 +1,56 @@
-import './Register.css';
+import RegisterForm from "../../Components/RegisterForm";
+import {Grid, CssBaseline, Typography, Box} from "@mui/material"
+import loginbg from '../../assets/login-bg.jpg';
+import bungee from '../../Fonts/Bungee-Regular.ttf'
+import DeveloperBoardIcon from '@mui/icons-material/DeveloperBoard';
 
 const Register = () => {
+
+    const globalStyles = `
+        @font-face {
+            font-family: 'Bungee Regular';
+            src: url(${bungee}) format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }
+    `;
+
     return (
-        <div className="register-container">
-            <p>This will be the register page</p>
-        </div>
+        <>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+                <Grid item 
+                   xs={false} 
+                   sm={5} 
+                   md={7}
+                   sx={{backgroundImage: `url(${loginbg})`,
+                        backgroundRepeat: 'no-repeat',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }} 
+                >
+                    <Box 
+                        display={"flex"} 
+                        flexDirection={"column"} 
+                        maxWidth={700}
+                        alignItems={"center"}
+                        justifyContent={"center"}
+                        margin={"auto"}
+                        marginTop={35}
+                        sx={{backgroundColor: 'rgba(3, 12, 46, 0.3)'}}>
+                        <style>{globalStyles}</style>
+                        <Box display={"flex"} alignItems={"center"} padding={2} >
+                            <DeveloperBoardIcon sx={{color: 'white', opacity: 0.7, fontSize: 100}} />
+                            <Typography sx={{opacity: 0.8}} textAlign={'center'} fontFamily={'Bungee Regular'} fontSize={25} color={'white'}>
+                                Your journey in the world of tech is just about to begin</Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={7} md={5} elevation={6} square>
+                    <RegisterForm/>
+                </Grid>
+            </Grid>
+        </>
     );
 }
 
