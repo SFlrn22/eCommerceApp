f9c73b227fec00ffae9df2f6536b7c395749f9d5
SFlorin <stoflorin22@gmail.com>
2023-05-09 10:28:07 +0300

    Modify navbar using MUI

--- eCommerceApp-Frontend/src/App.jsx
+++ eCommerceApp-Frontend/src/App.jsx
@@ -5,8 +5,11 @@ import Home from './Routes/Home/Home.jsx'
 import Content from './Routes/Content/Content.jsx'
 import Login from './Routes/Login/Login.jsx'
 import Register from './Routes/Register/Register.jsx'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Box, Typography, AppBar, Button, Container } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom'
 
 export default function App() {
 
@@ -27,29 +30,67 @@ export default function App() {
       <RouterProvider router={router} />
     </div>
   );
-}
+};
 
 const Root = () => {
+  const navigate = useNavigate();
+  const isLogged = () => {
+    if(sessionStorage.getItem('token')) return true;
+    return false;
+  };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('username');
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('firstname');
+    sessionStorage.removeItem('lastname');
+    toast.success("Logged out")
+    navigate('/')
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#0954DA'
+      },
+      secondary: {
+        main: '#EE035F'
+      },
+    },
+  });
 
   return (
     <>
-      <div className='navbar'>
-        <div className='left-side'>
-          <span className='nav-title'>EShop</span>
-          <Link className='link' to="/"> Home </Link>
-          <Link className='link' to="/content"> Content </Link>
-        </div>
-        <div className='right-side'>
-          <Link className='link' to="/login"> Login </Link>
-          <Link className='link' to="/register"> Register </Link>
-        </div>
-      </div>
-      <div className='component-content'>
-        <Outlet />
-      </div>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static" sx={{height: 45}}>
+          <Box maxWidth="xl" display={"flex"} alignItems={'center'} justifyContent={'space-between'} >
+            <Box display={"flex"} gap={1} ml={4} mt={0.8}>
+              <Typography mt={0.15} mr={2}>eComm</Typography>
+              <Link className='link' to="/"> Home </Link>
+              <Link className='link' to="/content"> Content </Link>
+            </Box>
+            <Box display={"flex"} gap={1} mr={4} mt={0.7}>
+              {isLogged() ? (
+                <>
+                  <Button sx={{color: 'black'}} onClick={handleLogout}>Logout</Button>
+                </>
+              ) : (
+                <>
+                  <Link className='link' to="/login"> Login </Link>
+                  <Link className='link' to="/register"> Register </Link>
+                </>
+              )}
+            </Box>
+          </Box>
+        </AppBar>
+        <Box>
+          <Outlet />
+        </Box>
+      </ThemeProvider>
     </>
   )
-}
+};
 
 
 
